b7533399a682df667741f9dac8701e2de960b81c
soko-no-otaku <soko.no.otaku@gmail.com>
2017-05-02 22:07:24 +0900

    現在時刻の書式を変更

--- init.el
+++ init.el
@@ -32,6 +32,9 @@
 ;; 行番号・桁番号をモードラインに表示する・しない設定
 (line-number-mode t)   ; 行番号。tなら表示、nilなら非表示
 (column-number-mode t) ; 桁番号。tなら表示、nilなら非表示
+
+;; 現在時刻の書式
+(setq display-time-format "%m/%d %H:%M")
 ;; モードラインに現在時刻を表示する
 (display-time)
 
