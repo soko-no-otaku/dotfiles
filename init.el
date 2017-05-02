947c14e15ae40a0dc2b596fddaad0b83754ae756
soko-no-otaku <soko.no.otaku@gmail.com>
2017-05-02 23:35:04 +0900

    auto-complete関係

--- init.el
+++ init.el
@@ -82,6 +82,17 @@
 ;; フォント設定(よく分かってない)
 (set-face-attribute 'default nil :family "Rounded M+ 1m" :height 220)
 
+;; auto-complete
+(require 'auto-complete-config)
+(ac-config-default)
+(add-to-list 'ac-modes 'text-mode)         ;; text-modeでも自動的に有効にする
+(add-to-list 'ac-modes 'fundamental-mode)  ;; fundamental-mode
+(add-to-list 'ac-modes 'org-mode)
+(add-to-list 'ac-modes 'yatex-mode)
+(ac-set-trigger-key "TAB")
+(setq ac-use-menu-map t)       ;; 補完メニュー表示時にC-n/C-pで補完候補選択
+(setq ac-use-fuzzy t)          ;; 曖昧マッチ
+
 ;; より下に記述した物が PATH の先頭に追加されます
 (dolist (dir (list
               "/sbin"
